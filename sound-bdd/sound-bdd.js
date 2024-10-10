c60ff5ce52c99a0833d4f5dbabae3efc89780728
rmnlefort <r-lefort@orange.fr>
2024-10-10 15:21:51 +0200

    Add files via upload

---
+++ sound-bdd/sound-bdd.js
@@ -0,0 +1,163 @@
+const objects = [  
+    {
+        "label":"Je vais le faire",
+        "path":"je-vais-faire.mp3"
+    },
+    {
+        "label":"OSS 117 - rire 1",
+        "path":"oss-117-rire-1.mp3"
+    },
+    {
+        "label":"Sacré Hubert",
+        "path":"sacre-hubert.mp3"
+    },
+    {
+        "label":"You're so french",
+        "path":"youre-so-french.mp3"
+    },
+    {
+        "label":"Merci la gueuse",
+        "path":"les-visiteurs-merci.mp3"
+    }, 
+    {
+        "label":"C'est une catastrophe !",
+        "path":"les-visiteurs-catastrophe.mp3"
+    }, 
+    {
+        "label":"Okay !",
+        "path":"les-visiteurs-ok.mp3"
+    },
+    {
+        "label":"Il s'appelle Juste Leblanc",
+        "path":"juste.mp3"
+    },
+    {
+        "label":"Tu veux manger quoi ?",
+        "path":"manger-quoi.mp3"
+    },
+    {
+        "label":"Une ouiche lorraine",
+        "path":"ouiche-lorraine.mp3"
+    },
+    {
+        "label":"Des chips",
+        "path":"chips.mp3"
+    },
+    {
+        "label":"J'ai faim",
+        "path":"jai-faim.mp3"
+    },
+    {
+        "label":"Croque des concombres",
+        "path":"croque-concombre.mp3"
+    },
+    {
+        "label":"Bon maintenant t'arrêtes !",
+        "path":"la-megachiasse.mp3"
+    },
+    {
+        "label":"Bien manger c'est important",
+        "path":"manger-important.mp3"
+    },
+    {
+        "label":"Un fasciste de merde",
+        "path":"fasciste-de-merde.mp3"
+    },
+    {
+        "label":"Terrorisme, grand banditisme",
+        "path":"terrorisme.mp3"
+    },
+    {
+        "label":"Mobilisation générale à Rennes",
+        "path":"stup-rennes.mp3"
+    },
+    {
+        "label":"La moindre erreur ne pardonne pas",
+        "path":"erreur.mp3"
+    },
+    {
+        "label":"AH !",
+        "path":"denis-brogniart-ah.mp3"
+    },
+    {
+        "label":"Ouille ouille ouille",
+        "path":"ouille-ouille.mp3"
+    },
+    {
+        "label":"Au revoir",
+        "path":"au-revoir.mp3"
+    },
+    {
+        "label":"C'est contre nature",
+        "path":"contre-nature.mp3"
+    },
+    {
+        "label":"Mais t'es pas net !",
+        "path":"tes-pas-net.mp3"
+    },
+    {
+        "label":"Ça va pas Baptiste",
+        "path":"ca-va-pas-baptiste.mp3"
+    },
+    {
+        "label":"New York Unité Spéciale",
+        "path":"law-order.mp3"
+    },
+    {
+        "label":"Ah bah bravo Nils !",
+        "path":"ah-bah-bravo-nils.mp3"
+    },
+    {
+        "label":"Quelle indignité !",
+        "path":"quelle-indignite.mp3"
+    },
+    {
+        "label":"Olivier !",
+        "path":"olivier.mp3"
+    },
+    {
+        "label":"Ils sont là",
+        "path":"ils-sont-la.mp3"
+    },
+    {
+        "label":"Caca chez Paul",
+        "path":"je-vais-faire-caca.mp3"
+    },
+    {
+        "label":"Pas le choix",
+        "path":"pas-le-choix.mp3"
+    },
+    {
+        "label":"J'ai pas le temps",
+        "path":"pas-le-temps.mp3"
+    },
+    {
+        "label":"Calme-toi Jérémie",
+        "path":"calme_toi_jeremie.mp3"
+    },
+    {
+        "label":"Brice sauve moi !",
+        "path":"bricesauvemoi.mp3"
+    },
+    {
+        "label":"Je m'appelle Brice",
+        "path":"jemappellebrice.mp3"
+    },
+    {
+        "label":"C'est pas cool",
+        "path":"cest_pas_cool.mp3"
+    },
+    {
+        "label":"L'alcool c'est pas cool",
+        "path":"lalcool_cest_pas_cool.mp3"
+    },
+    {
+        "label":"T'as regardé (mère)",
+        "path":"tas-regarde-cul-mere.mp3"
+    },
+    {
+        "label":"T'as regardé (père)",
+        "path":"tas-regarde-cul-pere.mp3"
+    }
+    
+]
